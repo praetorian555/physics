d60b3b3fd2a9055b7f85ee3739f660baedeabd8c
Marko Kostic <archmarko92@gmail.com>
2023-01-21 19:43:39 +0000

    Initial setup

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,29 @@
+find_program(CLANG_FORMAT "clang-format")
+
+if (CLANG_FORMAT)
+
+	file(GLOB_RECURSE FILES_TO_FORMAT
+		${PROJECT_SOURCE_DIR}/include/*.h
+		${PROJECT_SOURCE_DIR}/include/*.hpp
+		${PROJECT_SOURCE_DIR}/src/*.c
+		${PROJECT_SOURCE_DIR}/src/*.cpp
+		${PROJECT_SOURCE_DIR}/src/*.h
+		${PROJECT_SOURCE_DIR}/src/*.hpp
+		${PROJECT_SOURCE_DIR}/tests/*.c
+		${PROJECT_SOURCE_DIR}/tests/*.cpp)
+
+
+	add_custom_target(
+        clang-format
+        COMMAND clang-format
+        -i
+        -style=file
+        ${FILES_TO_FORMAT}
+    )
+	set_target_properties(clang-format PROPERTIES FOLDER CustomCommands)
+
+else()
+
+    message(WARNING "clang-format not found, clang-format target will not work!")
+
+endif()
---
+++ tests/particle_test.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "physics/particle.h"
+
+TEST_CASE("Init", "Particle")
+{
+    REQUIRE(true == true);
+}
