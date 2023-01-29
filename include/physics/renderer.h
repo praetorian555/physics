322005e0f6210af5e8d92f0393330bb32eadeeed
Marko Kostic <archmarko92@gmail.com>
2023-01-29 20:01:45 +0000

    Add rndr dependency and empty renderer class

---
+++ include/physics/renderer.h
@@ -0,0 +1,8 @@
+//
+// Created by Marko on 29/01/2023.
+//
+
+#ifndef PHYSICS_RENDERER_H
+#define PHYSICS_RENDERER_H
+
+#endif //PHYSICS_RENDERER_H
---
+++ src/renderer.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Marko on 29/01/2023.
+//
