80db4d9ec570849f9eb779b2aa416692aaee6f0d
Marko Kostic <archmarko92@gmail.com>
2023-01-30 20:42:34 +0000

    Add tests for ParticleDrag and ParticleForceRegistry

--- tests/particle_test.cpp
+++ tests/particle_test.cpp
@@ -84,7 +84,8 @@ TEST_CASE("Gravity", "Particle")
     Particle.SetMass(PHYSICS_REALC(1.0));
     Particle.SetDamping(PHYSICS_REALC(0.5));
     Particle.SetVelocity(math::Vector3(PHYSICS_REALC(4.0), PHYSICS_REALC(5.0), PHYSICS_REALC(6.0)));
-    physics::ParticleGravity Gravity(math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+    physics::ParticleGravity Gravity(
+        math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
     REQUIRE(Gravity.GetGravity() ==
             math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
     Gravity.UpdateForce(Particle, PHYSICS_REALC(1.0));
@@ -97,4 +98,62 @@ TEST_CASE("Gravity", "Particle")
             math::Vector3(PHYSICS_REALC(2.5), PHYSICS_REALC(3.5), PHYSICS_REALC(4.5)));
     REQUIRE(Particle.GetAcceleration() ==
             math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+}
+
+TEST_CASE("Drag", "Particle")
+{
+    // Write a test for ParticleDrag in Particle.
+    physics::Particle Particle;
+    Particle.SetMass(PHYSICS_REALC(1.0));
+    Particle.SetDamping(PHYSICS_REALC(0.5));
+    Particle.SetVelocity(math::Vector3(PHYSICS_REALC(0.0), PHYSICS_REALC(0.0), PHYSICS_REALC(4.0)));
+    physics::ParticleDrag Drag(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0));
+    REQUIRE(Drag.GetK1() == PHYSICS_REALC(1.0));
+    REQUIRE(Drag.GetK2() == PHYSICS_REALC(2.0));
+    Drag.UpdateForce(Particle, PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetForceAccumulator() ==
+            math::Vector3(PHYSICS_REALC(0.0), PHYSICS_REALC(0.0), PHYSICS_REALC(-36.0)));
+    Particle.Integrate(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetPosition() ==
+            math::Point3(PHYSICS_REALC(0.0), PHYSICS_REALC(0.0), PHYSICS_REALC(4.0)));
+    REQUIRE(Particle.GetVelocity() ==
+            math::Vector3(PHYSICS_REALC(0.0), PHYSICS_REALC(0.0), PHYSICS_REALC(-16.0)));
+    REQUIRE(Particle.GetAcceleration() ==
+            math::Vector3(PHYSICS_REALC(0.0), PHYSICS_REALC(0.0), PHYSICS_REALC(-36.0)));
+}
+
+TEST_CASE("ForceRegistry", "Particle")
+{
+    physics::Particle Particle;
+    Particle.SetMass(PHYSICS_REALC(1.0));
+    Particle.SetDamping(PHYSICS_REALC(0.5));
+    Particle.SetVelocity(math::Vector3(PHYSICS_REALC(4.0), PHYSICS_REALC(5.0), PHYSICS_REALC(6.0)));
+    physics::ParticleGravity Gravity(
+        math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+    physics::ParticleForceRegistry Registry;
+    Registry.Add(&Particle, &Gravity);
+    Registry.UpdateForces(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetForceAccumulator() ==
+            math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+    Particle.Integrate(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetPosition() ==
+            math::Point3(PHYSICS_REALC(4.0), PHYSICS_REALC(5.0), PHYSICS_REALC(6.0)));
+    REQUIRE(Particle.GetVelocity() ==
+            math::Vector3(PHYSICS_REALC(2.5), PHYSICS_REALC(3.5), PHYSICS_REALC(4.5)));
+    REQUIRE(Particle.GetAcceleration() ==
+            math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+
+    Registry.Clear();
+    Registry.UpdateForces(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetForceAccumulator() == math::Vector3());
+
+    Registry.Add(&Particle, &Gravity);
+    Registry.UpdateForces(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetForceAccumulator() ==
+            math::Vector3(PHYSICS_REALC(1.0), PHYSICS_REALC(2.0), PHYSICS_REALC(3.0)));
+    Particle.Integrate(PHYSICS_REALC(1.0));
+
+    Registry.Remove(&Particle, &Gravity);
+    Registry.UpdateForces(PHYSICS_REALC(1.0));
+    REQUIRE(Particle.GetForceAccumulator() == math::Vector3());
 }
