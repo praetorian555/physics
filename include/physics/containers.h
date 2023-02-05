127f79d588cc8a70277b222cf41d785d0f89f14e
Marko Kostic <archmarko92@gmail.com>
2023-02-05 16:09:33 +0000

    Add containers.h to encapsulate containers used

---
+++ include/physics/containers.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <array>
+#include <span>
+#include <vector>
+
+#include "physics/base.h"
+
+namespace physics
+{
+template <typename T>
+using Array = std::vector<T>;
+
+template <typename T, size_t N>
+using StackArray = std::array<T, N>;
+
+template <typename T>
+using Span = std::span<T>;
+}  // namespace physics
