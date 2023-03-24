898e570ba106a86c2c6d1e11a13277baf60c9218
Marko Kostic <archmarko92@gmail.com>
2023-03-24 17:25:54 +0000

    Implement broad phase using bounding volume hierarchy

--- include/physics/collide-broad.h
+++ include/physics/collide-broad.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "physics/containers.h"
+#include "physics/shapes-helpers.h"
+#include "physics/shapes-overlaps.h"
 
 namespace Physics
 {
@@ -24,7 +26,7 @@ namespace BroadPhase
  * @tparam BoundingVolume Type of the bounding volume used.
  */
 template <typename BoundingVolume>
-class BVHNode
+struct BVHNode
 {
     BVHNode* left = nullptr;
     BVHNode* right = nullptr;
@@ -35,12 +37,18 @@ class BVHNode
     /** If node doesn't have a body its not a leaf node. */
     RigidBody* body = nullptr;
 
+    BVHNode(BVHNode* in_parent, const BoundingVolume& in_bounding_volume, RigidBody* in_body);
+
     /**
      * Removes the node from the hierarchy and recalculates the bounding volume of the parent node.
      */
     ~BVHNode();
 
-    bool IsLeaf() const { return body != nullptr; }
+    /**
+     * Check if the node is a leaf node, which means it has a rigid body.
+     * @return Returns true if the node is a leaf node, false otherwise.
+     */
+    [[nodiscard]] bool IsLeaf() const { return body != nullptr; }
 
     /**
      * Insert a rigid body into the bounding volume hierarchy.
@@ -61,20 +69,186 @@ class BVHNode
  */
 template <typename BoundingVolume>
 int GetPotentialContacts(BVHNode<BoundingVolume>* in_root_node,
-                         Array<PotentialContact>& out_contacts,
-                         int in_capacity);
-
-}
+                         Array<PotentialContact>& out_contacts);
 
 // Private function declarations ///////////////////////////////////////////////////////////////////
 
-namespace BroadPhasePrivate
+namespace Private
 {
 
-}
+template <typename BoundingVolume>
+bool ShouldDescendLeft(BVHNode<BoundingVolume>* left, BVHNode<BoundingVolume>* right);
+
+template <typename BoundingVolume>
+bool ShouldInsertLeft(BVHNode<BoundingVolume>* left,
+                      BVHNode<BoundingVolume>* right,
+                      const BoundingVolume& new_volume);
+
+/**
+ * Recalculate the bounding volume of the node and all its parents.
+ * @tparam BoundingVolume Type of the bounding volume.
+ * @param node Node to recalculate the bounding volume of.
+ */
+template <typename BoundingVolume>
+void RecalculateBoundingVolume(BVHNode<BoundingVolume>* node);
+
+}  // namespace Private
+
+}  // namespace BroadPhase
+
+}  // namespace Physics
 
 // Template implementations ////////////////////////////////////////////////////////////////////////
 
+template <typename BoundingVolume>
+Physics::BroadPhase::BVHNode<BoundingVolume>::BVHNode(BVHNode* in_parent,
+                                                      const BoundingVolume& in_bounding_volume,
+                                                      RigidBody* in_body)
+    : parent(in_parent), bounding_volume(in_bounding_volume), body(in_body)
+{
+}
 
+template <typename BoundingVolume>
+Physics::BroadPhase::BVHNode<BoundingVolume>::~BVHNode()
+{
+    if (parent != nullptr)
+    {
+        BVHNode* sibling = parent->left == this ? parent->right : parent->left;
+        parent->left = sibling->left;
+        parent->right = sibling->right;
+        parent->body = sibling->body;
+        parent->bounding_volume = sibling->bounding_volume;
+        sibling->parent = nullptr;
+        sibling->left = nullptr;
+        sibling->right = nullptr;
+        delete sibling;
+    }
 
-}  // namespace Physics
+    if (left != nullptr)
+    {
+        left->parent = nullptr;
+        delete left;
+    }
+    if (right != nullptr)
+    {
+        right->parent = nullptr;
+        delete right;
+    }
+}
+
+template <typename BoundingVolume>
+void Physics::BroadPhase::BVHNode<BoundingVolume>::Insert(RigidBody* in_body,
+                                                          const BoundingVolume& in_bounding_volume)
+{
+    BVHNode* node = this;
+    while (node != nullptr)
+    {
+        if (node->IsLeaf())
+        {
+            left = new BVHNode<BoundingVolume>(this, bounding_volume, body);
+            right = new BVHNode<BoundingVolume>(this, in_bounding_volume, in_body);
+            body = nullptr;
+            bounding_volume = Enclose(bounding_volume, in_bounding_volume);
+            return;
+        }
+        if (Private::ShouldInsertLeft(node->left, node->right, in_bounding_volume))
+        {
+            node = node->left;
+        }
+        else
+        {
+            node = node->right;
+        }
+    }
+}
+
+template <typename BoundingVolume>
+int Physics::BroadPhase::GetPotentialContacts(BVHNode<BoundingVolume>* in_root_node,
+                                              Array<PotentialContact>& out_contacts)
+{
+    if (in_root_node == nullptr || in_root_node->IsLeaf())
+    {
+        return 0;
+    }
+    if (in_root_node->left == nullptr || in_root_node->right == nullptr)
+    {
+        return 0;
+    }
+
+    struct Pair
+    {
+        BVHNode<BoundingVolume>* left;
+        BVHNode<BoundingVolume>* right;
+    };
+    Array<Pair> stack;
+    stack.reserve(32);
+    stack.push_back({in_root_node->left, in_root_node->right});
+    while (!stack.empty())
+    {
+        Pair pair = stack.back();
+        stack.pop_back();
+
+        if (!Overlaps(pair.left->bounding_volume, pair.right->bounding_volume))
+        {
+            continue;
+        }
+        if (pair.left->IsLeaf() && pair.right->IsLeaf())
+        {
+            out_contacts.push_back({pair.left->body, pair.right->body});
+            continue;
+        }
+        if (Private::ShouldDescendLeft(pair.left, pair.right))
+        {
+            stack.push_back({pair.left->right, pair.right});
+            stack.push_back({pair.left->left, pair.right});
+        }
+        else
+        {
+            stack.push_back({pair.left, pair.right->right});
+            stack.push_back({pair.right, pair.right->left});
+        }
+    }
+}
+
+template <typename BoundingVolume>
+bool Physics::BroadPhase::Private::ShouldDescendLeft(BVHNode<BoundingVolume>* left,
+                                                     BVHNode<BoundingVolume>* right)
+{
+    return right->IsLeaf() ||
+           (left->bounding_volume->GetVolume() >= right->bounding_volume->GetVolume());
+}
+
+template <typename BoundingVolume>
+bool Physics::BroadPhase::Private::ShouldInsertLeft(BVHNode<BoundingVolume>* left,
+                                                    BVHNode<BoundingVolume>* right,
+                                                    const BoundingVolume& new_volume)
+{
+    const BoundingVolume left_volume = Enclose(left->bounding_volume, new_volume);
+    const BoundingVolume right_volume = Enclose(right->bounding_volume, new_volume);
+    return left_volume.GetVolume() < right_volume.GetVolume();
+}
+
+template <typename BoundingVolume>
+void Physics::BroadPhase::Private::RecalculateBoundingVolume(BVHNode<BoundingVolume>* node)
+{
+    if (node == nullptr || node->IsLeaf())
+    {
+        return;
+    }
+
+    Array<BVHNode<BoundingVolume>*> stack;
+    stack.reserve(16);
+    stack.push_back(node);
+    while (!stack.empty())
+    {
+        BVHNode<BoundingVolume>* current = stack.back();
+        stack.pop_back();
+
+        current->bounding_volume =
+            Enclose(current->left->bounding_volume, current->right->bounding_volume);
+        if (current->parent != nullptr)
+        {
+            stack.push_back(current->parent);
+        }
+    }
+}
