2a001baeb2c4fdcae2849dde0115056c0d99c6e1
Marko Kostic <archmarko92@gmail.com>
2025-12-22 22:20:26 +0100

    [Scene] Add scene skeleton

---
+++ include/physics/scene.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Korisnik on 22/12/2025.
+//
+
+#ifndef PHYSICS_SCENE_HPP
+#define PHYSICS_SCENE_HPP
+
+#endif //PHYSICS_SCENE_HPP
---
+++ src/scene.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Korisnik on 22/12/2025.
+//
