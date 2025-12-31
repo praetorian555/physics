bd434c1d8a6a604b97e0a8abc4f5e58c135d82f3
Marko Kostic <archmarko92@gmail.com>
2025-12-31 13:34:37 +0100

    [SampleSpheres] Properly implement ResetSimulation, increase the ground

--- samples/sample-spheres/sample-spheres.cpp
+++ samples/sample-spheres/sample-spheres.cpp
@@ -32,16 +32,16 @@ public:
             }
         }
 
-        Opal::RNG rng;
-
         // Static Floor
-        for (Physics::i32 x = 0; x < 3; ++x)
+        for (Physics::i32 x = 0; x < 6; ++x)
         {
-            for (Physics::i32 z = 0; z < 3; ++z)
+            for (Physics::i32 z = 0; z < 6; ++z)
             {
                 Physics::Body ground;
                 const Physics::real radius = 80;
-                ground.position = Physics::Vector3r{(x - 1) * radius * 0.25f, -radius + 2.0f, (z - 1) * radius * 0.25f - 10};
+                const Physics::real xx = static_cast<Physics::real>(x - 3) * radius * 0.25f;
+                const Physics::real zz = static_cast<Physics::real>(z - 3) * radius * 0.25f;
+                ground.position = Physics::Vector3r{xx, -radius + 2.0f, zz - 10};
                 ground.orientation = Physics::Quatr::FromAxisAngleDegrees(Physics::Vector3r{1, 0, 0}, 45.0f);
                 ground.inverse_mass = 0.0f;
                 ground.elasticity = 0.99f;
@@ -52,34 +52,13 @@ public:
                 m_shapes.PushBack(std::move(ground_sphere));
             }
         }
-
-        PauseSimulation();
     }
 
     void ResetSimulation() override
     {
         SampleApp::ResetSimulation();
         m_shapes.Clear();
-
-        Opal::AllocatorBase* allocator = Opal::GetDefaultAllocator();
-
-        Physics::Body body;
-        body.position = Physics::Vector3r{0, 10, -10};
-        body.orientation = Physics::Quatr::Identity();
-        body.inverse_mass = 1.0f;
-        Opal::ScopePtr<Physics::SphereShape> sphere(allocator, PHYSICS_CONST(1.0));
-        body.shape = sphere.Get();
-        AddBody(body);
-        m_shapes.PushBack(std::move(sphere));
-
-        Physics::Body ground;
-        ground.position = Physics::Vector3r{0, -998, -10};
-        ground.orientation = Physics::Quatr::FromAxisAngleDegrees(Physics::Vector3r{1, 0, 0}, 45.0f);
-        ground.inverse_mass = 0.0f;
-        Opal::ScopePtr<Physics::SphereShape> ground_sphere(allocator, PHYSICS_CONST(1000.0));
-        ground.shape = ground_sphere.Get();
-        AddBody(ground);
-        m_shapes.PushBack(std::move(ground_sphere));
+        SetupSimulation();
     }
 
 private:
